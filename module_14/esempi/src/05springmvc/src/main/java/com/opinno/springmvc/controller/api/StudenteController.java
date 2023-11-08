745d70e189c7978fd48a1be856a5a99ada84ac45
Davide <149406446+dfellaib@users.noreply.github.com>
2023-11-08 10:33:28 +0100

    Aggiunte richieste RequestParam, PathVariable e RequestBody

--- module_14/esempi/src/05springmvc/src/main/java/com/opinno/springmvc/controller/api/StudenteController.java
+++ module_14/esempi/src/05springmvc/src/main/java/com/opinno/springmvc/controller/api/StudenteController.java
@@ -1,12 +1,11 @@
 package com.opinno.springmvc.controller.api;
 
 import com.opinno.springmvc.model.Studente;
+import com.opinno.springmvc.model.http.RequestHelloWorld;
 import com.opinno.springmvc.service.StudenteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -19,11 +18,32 @@ public class StudenteController {
     @Autowired
     StudenteService studenteService;
 
-    @GetMapping("/helloworld")
-    public ResponseEntity<String> helloWorld() {
+    @GetMapping("/helloworld/{id}")
+    public ResponseEntity<String> helloWorldPathVar(@PathVariable Long id) {
+
+        System.out.println(id);
 
         return new ResponseEntity<>("Hello World", HttpStatus.OK);
     }
+
+    @GetMapping("/helloworld")
+    public ResponseEntity<String> helloWorldRequestParam(@RequestParam Long id) {
+
+        System.out.println(id);
+
+        return new ResponseEntity<>("Hello World", HttpStatus.OK);
+    }
+
+    // DTO --> Data Transfer Object (Pattern)
+    @PostMapping("/helloworld")
+    public ResponseEntity<String> helloWorldBodyParam(@RequestBody RequestHelloWorld requestHelloWorld) {
+
+        System.out.println(requestHelloWorld);
+
+        return new ResponseEntity<>("Hello World", HttpStatus.OK);
+    }
+
+
 
     @GetMapping("/studenti")
     public ResponseEntity<List<Studente>> getAllStudenti() {
---
+++ module_14/esempi/src/05springmvc/src/main/java/com/opinno/springmvc/model/http/RequestHelloWorld.java
@@ -0,0 +1,13 @@
+package com.opinno.springmvc.model.http;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestHelloWorld {
+
+    private String key;
+}
